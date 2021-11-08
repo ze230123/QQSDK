6707c7d61e1580ac3b7befaaffc68d7a78635dec
泽i <zhangzh@eagersoft.cn>
2021-11-08 11:45:39 +0800

    Initial Commit

---
+++ Lib/Headers/QQSDK.h
@@ -0,0 +1,25 @@
+//
+//  QQSDK.h
+//  QQSDK
+//
+//  Created by youzy01 on 2021/11/8.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QQSDK.
+FOUNDATION_EXPORT double QQSDKVersionNumber;
+
+//! Project version string for QQSDK.
+FOUNDATION_EXPORT const unsigned char QQSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QQSDK/PublicHeader.h>
+//#import "QQApiInterface.h"
+//#import "QQApiInterfaceObject.h"
+//#import "SDKDef.h"
+//#import "TencentOAuth.h"
+
+#import <QQSDK/QQApiInterface.h>
+#import <QQSDK/QQApiInterfaceObject.h>
+#import <QQSDK/SDKDef.h>
+#import <QQSDK/TencentOAuth.h>
